5c92228420cfd73b974c83d0234c248b60e51651
faiaz <fayaz.sanaulla@gmail.com>
2016-12-17 17:56:12 +0200

    expansion

--- src/main/scala/components/Components.scala
+++ src/main/scala/components/Components.scala
@@ -1,22 +1,25 @@
-package main.scala.components
+package components
 
 import java.text.NumberFormat
-import java.util.Locale
 
+import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
 import scalafx.scene.control.Alert.AlertType
 import scalafx.scene.control._
+import scalafx.scene.image.{Image, ImageView}
 import scalafx.util.converter.FormatStringConverter
-import scalafx.Includes._
 
 /**
   * Created by faiaz on 15.12.16.
   */
 trait Components {
 
-  val currencyFormat = NumberFormat.getCurrencyInstance(Locale.US)
+  val currencyFormat = NumberFormat.getNumberInstance()
   val converter = new FormatStringConverter[Number](currencyFormat)
 
-  lazy val slider = new Slider(0, 100, 0)
+  lazy val slider = new Slider(0, 100, 0) {
+    minWidth = 400.0
+  }
 
   lazy val textFiled = new TextField {
     textFormatter = new TextFormatter (converter) {
@@ -26,5 +29,25 @@ trait Components {
 
   lazy val button = new Button("Click"){
     onAction = handle { new Alert(AlertType.Information, "Info Message").showAndWait() }
+  }
+
+  lazy val imageView = new ImageView {
+    // image = new Image(getClass.getResource("epsya.jpg").toExternalForm)
+       image = new Image("file:image.png")
+    // image = new Image("http://www.scala-lang.org/resources/img/scala-logo.png")
+  }
+
+  lazy val choiseBox = new ChoiceBox[String] {
+    maxWidth = 80
+    maxHeight = 50
+    items = ObservableBuffer("Test1", "Test2", "Test3")
+    selectionModel().selectFirst()
+    selectionModel().selectedItem.onChange(
+      (_, _, newValue) => newValue match {
+        case "Test1" => textFiled.setText("4")
+        case "Test2" => textFiled.setText("5")
+        case "Test3" => textFiled.setText("6")
+      }
+    )
   }
 }
--- src/main/scala/root/MainFrame.scala
+++ src/main/scala/root/MainFrame.scala
@@ -1,12 +1,12 @@
-package main.scala.root
+package root
 
-import main.scala.components.Components
+import components.Components
 
 import scalafx.application.JFXApp
+import scalafx.geometry.Pos
 import scalafx.scene.Scene
 import scalafx.scene.layout.GridPane
 import scalafx.stage.Screen
-
 
 /**
   * Created by faiaz on 15.12.16.
@@ -23,11 +23,13 @@ object MainFrame extends JFXApp with Components {
       resizable = false
 
       root = new GridPane {
-        hgap = 10
-        vgap = 10
-        add(slider, 10, 10)
-        add(textFiled, 20, 20)
-        add(button, 15, 15)
+        alignment = Pos.Center
+        hgap = 5
+        vgap = 5
+        add(slider, 10, 100)
+        add(textFiled, 11, 100)
+        add(choiseBox, 4, 4)
+        add(imageView, 1, 1)
       }
     }
   }
